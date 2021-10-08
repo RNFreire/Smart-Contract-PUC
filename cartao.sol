8b707fa19a05deffd95f9ececc44611cf8494d98
RNFreire <92129675+RNFreire@users.noreply.github.com>
2021-10-07 21:41:41 -0300

    novo cartao.sol

---
+++ cartao.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.8.9;
+
+contract CartaoDeVacinacao {
+    //autor: Rafael
+    string constant public nome = "Rafael";
+    bool public imunizado = true;
+    string public unidade = "brooklin";
+    uint public CNES = 100000000000;
+    uint public data = 20210712;
+    uint private lote = 10000000;
+    string constant private fabricante = "Pfizer";
+    string public vacinador = "Joana";
+    address public wallet = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+   }
